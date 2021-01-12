e6841ae0ef227e0bcedf3f748092d56854e7cb80
Michele Locati <michele@locati.it>
2021-01-12 17:00:06 +0100

    vue add bootstrap-vue

--- web/main.ts
+++ web/main.ts
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import { BootstrapVue } from "bootstrap-vue";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 
