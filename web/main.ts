509a51b49cc147850d4a63303130de25e506f298
Michele Locati <michele@locati.it>
2021-01-12 17:12:41 +0100

    Add custom main.scss file

--- web/main.ts
+++ web/main.ts
@@ -4,6 +4,7 @@ import { BootstrapVue } from "bootstrap-vue";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import "./app.scss";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
