160b72e1e419a3682dad59c2085b22412c1e7e9b
Michele Locati <michele@locati.it>
2021-01-13 12:34:55 +0100

    Run lint

--- vue.config.js
+++ vue.config.js
@@ -1,35 +1,35 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+/* eslint-disable @typescript-eslint/no-var-requires */
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+/* eslint-enable @typescript-eslint/no-var-requires */
 
 module.exports = {
-    productionSourceMap: false,
-    lintOnSave: true,
-    outputDir: 'docs',
-    publicPath: '.',
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@': __dirname + '/web'
-            }
-        },
-        entry: {
-            app: './web/main.ts'
-        },
-        performance: {
-            maxEntrypointSize: 1048576,
-            maxAssetSize: 1048576
-        },
-        plugins: [
-            new CopyWebpackPlugin([
-                { from: __dirname + '/.nojekyll', to: __dirname + '/docs/' }
-            ])
-        ]
+  productionSourceMap: false,
+  lintOnSave: true,
+  outputDir: "docs",
+  publicPath: ".",
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": __dirname + "/web"
+      }
     },
-    chainWebpack: config => {
-        config
-            .plugin('html')
-            .tap(args => {
-                args[0].template =  __dirname + '/web/static/index.html'
-                return args
-            })
-    }
-}
+    entry: {
+      app: "./web/main.ts"
+    },
+    performance: {
+      maxEntrypointSize: 1048576,
+      maxAssetSize: 1048576
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: __dirname + "/.nojekyll", to: __dirname + "/docs/" }
+      ])
+    ]
+  },
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].template = __dirname + "/web/static/index.html";
+      return args;
+    });
+  }
+};
